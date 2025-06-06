a701f0992bba88ad8851850eec93772f74aebae2
TabarBaptiste <98088651+TabarBaptiste@users.noreply.github.com>
2025-06-06 15:35:56 +0200

    Suppression référence à importation Excel

--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Piece
-from .forms import PieceForm, ImportExcelForm
+# from .forms import PieceForm, ImportExcelForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db.models import Q
-import openpyxl
+# import openpyxl
 from django.http import HttpResponse
 
 def est_admin(user):
@@ -107,42 +107,42 @@ def supprimer_piece(request, pk):
         return redirect('stock:liste_pieces')
     return render(request, 'stock/confirm_delete.html', {'piece': piece})
 
-def import_excel(request):
-    try:
-        if request.method == 'POST' and request.FILES['excel_file']:
-            # On récupère le fichier Excel
-            excel_file = request.FILES['excel_file']
+# def import_excel(request):
+#     try:
+#         if request.method == 'POST' and request.FILES['excel_file']:
+#             # On récupère le fichier Excel
+#             excel_file = request.FILES['excel_file']
             
-            # On charge le fichier Excel
-            wb = openpyxl.load_workbook(excel_file)
-            sheet = wb.active  # On récupère la première feuille
+#             # On charge le fichier Excel
+#             wb = openpyxl.load_workbook(excel_file)
+#             sheet = wb.active  # On récupère la première feuille
 
-            # On parcourt toutes les lignes de la feuille
-            for row in sheet.iter_rows(min_row=2, values_only=True):  # On ignore la première ligne (les entêtes)
-                designation = row[0]
-                voiture = row[1]
-                quantite = row[2]
-                prix_vente = row[3]
-                prix_vente_reduction = row[4]
-                disponible = row[5].lower() == 'oui'  # Si "oui", on considère la pièce disponible
+#             # On parcourt toutes les lignes de la feuille
+#             for row in sheet.iter_rows(min_row=2, values_only=True):  # On ignore la première ligne (les entêtes)
+#                 designation = row[0]
+#                 voiture = row[1]
+#                 quantite = row[2]
+#                 prix_vente = row[3]
+#                 prix_vente_reduction = row[4]
+#                 disponible = row[5].lower() == 'oui'  # Si "oui", on considère la pièce disponible
 
-                # Crée une nouvelle instance de Piece et enregistre-la
-                Piece.objects.create(
-                    designation=designation,
-                    voiture=voiture,
-                    quantite=quantite,
-                    prix_vente=prix_vente,
-                    prix_vente_reduction=prix_vente_reduction,
-                    disponible=disponible
-                )
+#                 # Crée une nouvelle instance de Piece et enregistre-la
+#                 Piece.objects.create(
+#                     designation=designation,
+#                     voiture=voiture,
+#                     quantite=quantite,
+#                     prix_vente=prix_vente,
+#                     prix_vente_reduction=prix_vente_reduction,
+#                     disponible=disponible
+#                 )
 
-            # Rediriger vers la liste des pièces ou afficher un message de succès
-            return redirect('stock:liste_pieces')  # Change cette URL en fonction de ton app
+#             # Rediriger vers la liste des pièces ou afficher un message de succès
+#             return redirect('stock:liste_pieces')  # Change cette URL en fonction de ton app
 
-        else:
-            # Si ce n'est pas un POST, on affiche un formulaire vide
-            form = ImportExcelForm()
-            return render(request, 'stock/import_excel.html', {'form': form})
-    except Exception as e:
-    # Gérer l'exception et informer l'utilisateur
-        return HttpResponse(f"Une erreur s'est produite lors de l'importation : {str(e)}")
+#         else:
+#             # Si ce n'est pas un POST, on affiche un formulaire vide
+#             form = ImportExcelForm()
+#             return render(request, 'stock/import_excel.html', {'form': form})
+#     except Exception as e:
+#     # Gérer l'exception et informer l'utilisateur
+#         return HttpResponse(f"Une erreur s'est produite lors de l'importation : {str(e)}")
